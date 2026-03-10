9bebd672a029c0602ece10f4ecc765ec7dcabc6c
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-10 10:47:34 +0000

    Add initial project structure with routes, tests, and configuration files
    - Created console route for inspiring quotes
    - Added web route for the welcome view
    - Set up .gitignore files for storage directories
    - Implemented basic feature and unit tests
    - Configured Vite for asset management with Laravel

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Hash;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'password' => 'required|string'
+        ]);
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User registered Successfully',
+            'user'=> $user,
+            'token' => $token
+        ], 201);
+    }
+}

4c0d8b915c915df5206a50efb45da566a18a965c
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-12 00:42:46 +0000

    feat(reservation): add fillable attributes and casts for start_time and end_time

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -10,6 +10,22 @@ class Reservation extends Model
     /** @use HasFactory<\Database\Factories\ReservationFactory> */
     use HasFactory;
 
+    protected $fillable = [
+        'start_time',
+        'end_time',
+        'status',
+        'user_id',
+        'station_id',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'start_time' => 'datetime',
+            'end_time' => 'datetime',
+        ];
+    }
+
     public function station()
     {
         return $this->belongsTo(Station::class, 'station_id');

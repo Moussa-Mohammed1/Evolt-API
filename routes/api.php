e04ef803260f9b89f2d93e8ffbfc131b06487a4b
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-03-12 00:40:48 +0000

    chore(api): remove unnecessary blank line in routes file

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/stations', [StationController::class, 'index']);
-
+ 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/reservations', [ReservationController::class, 'index']);
     Route::post('/reservations', [ReservationController::class, 'store']);
